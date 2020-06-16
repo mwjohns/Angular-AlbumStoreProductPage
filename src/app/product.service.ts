34d6db9297ab860ad64aca7faa29fafb906c1b5d
mwjohns <mwjohns@bgsu.edu>
2020-06-16 08:26:53 -0400

    updated 2

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,14 +6,14 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-private _albumUrl;
+private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) {
-    this._albumUrl = '../assets/AlbumStorePage.json';
+    
    }
 
    getAlbum(id:number){
-     return this._http.get(this._albumUrl).map(x => Response.json());
+     return this._http.get(this._albumUrl).map((response) => response.json());
    }
 
 }
